4b559f58f7b01a34cb889d2ff1af8ce096c0c247
ninisnow <498671927@QQ.com>
2018-07-06 19:39:25 +0800

    修改

--- Scripts/test_001.py
+++ Scripts/test_001.py
@@ -1,3 +1,15 @@
+import allure,pytest
+
+
 class Test_001:
+    @allure.step(title = "登录")
     def test_001(self):
-        assert 0
+        allure.attach("用户名","输入用户名")
+        allure.attach("密码","输入密码")
+        allure.attach("登录","点击登录")
+        assert 1
+
+    @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
+    @allure.setp(title = "注册")
+    def test_02(self):
+        assert 1
